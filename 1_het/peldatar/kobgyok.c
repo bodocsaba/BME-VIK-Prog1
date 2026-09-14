b843774a3c8f9a1db5f4219054cc1e1b8eda4cc8
Bodó Csaba <115866118+bodocsaba@users.noreply.github.com>
2026-09-14 23:03:51 +0200

    Add files via upload

---
+++ 1_het/peldatar/kobgyok.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/*
+int main(){
+    printf("Kobgyok szamitas Newton es Heron modszerrel\n\n");
+    double szam;
+    printf("Add meg melyik szamnak keresed a kobgyoket\n\n");
+    printf("szam: ");
+    scanf("%lf",&szam);
+    double tipp = 1;
+    double ujtipp;
+    while(round((ujtipp*ujtipp*ujtipp)) != szam){
+        ujtipp = (szam /(tipp*tipp)+2*tipp)/3;
+        tipp = ujtipp;
+    }
+    printf("\n");
+    printf("Heron modszer: %g",ujtipp);
+    return 0;
+}
+*/
